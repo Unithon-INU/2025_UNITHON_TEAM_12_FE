e5dcbc361e690a2cdc852af0deb509d6509ef25a
blwxnhan <rabbitlin@naver.com>
2025-05-17 14:22:50 +0900

    Initial Commit

---
+++ Packit/PackitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackitApp.swift
+//  Packit
+//
+//  Created by Bowon Han on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PackitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
