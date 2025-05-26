2b51d29bd2b0ed50cd0f791c1ff3a52d1b82fb8b
blwxnhan <rabbitlin@naver.com>
2025-05-26 11:05:32 +0900

    [Feat] PlanPackit 제목 입력 화면 구현

---
+++ Packit/Component/PackitButton.swift
@@ -0,0 +1,31 @@
+//
+//  PackitButton.swift
+//  Packit
+//
+//  Created by Bowon Han on 5/26/25.
+//
+
+import SwiftUI
+
+struct PackitButton: View {
+    let title: String
+    
+    var body: some View {
+        Button(action: {}, label: {
+            Text(title)
+                .font(.custom("Pretendard", size: 20))
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+        })
+        .padding([.top, .bottom], 16)
+        .frame(maxWidth: .infinity)
+        .background {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.packitPurple)
+        }
+    }
+}
+
+#Preview {
+    PackitButton(title: "다음")
+}
--- Packit/View/PlanPackit/PlanPackitTitle.swift
+++ Packit/View/PlanPackit/PlanPackitTitle.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct PlanPackitTitle: View {
+    @State private var title: String = ""
+    
     var body: some View {
-        
+        VStack {
+            Text("이번 짐의 제목을 정해주세요!")
+                .font(.custom("Pretendard", size: 24))
+                .fontWeight(.bold)
+                .foregroundStyle(Color.packitPurple)
+                .padding(.top, 15)
+            
+            PackitTextField(text: $title, placeholder: Text("제목을 입력해주세요!"))
+                .padding([.leading, .trailing], 35)
+                .padding(.top, 30)
+            
+            Spacer()
+            
+            PackitButton(title: "다음")
+                .padding()
+        }
     }
 }
 
