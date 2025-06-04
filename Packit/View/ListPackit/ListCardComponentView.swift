31e3da234efb278e79eb88c30ccd7936bd61a131
yena0213 <yeana3131@naver.com>
2025-06-05 05:35:51 +0900

    [Feat] update ListCardComponentView-detail

--- Packit/View/ListPackit/ListCardComponentView.swift
+++ Packit/View/ListPackit/ListCardComponentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ListCardComponentView: View {
-    
+    @State var isBeen: Bool = false
     
     var body: some View {
         VStack(spacing: 7) {
             HStack{
-                Text(" D-1")
+                Text((isBeen ? "3월" : "D-1"))  //viewmodel.start_date(d-day how 처리?)
                     .font(.custom("Pretendard", size: 17))
                     .fontWeight(.light)
                     .foregroundColor(Color.packitLightText)
@@ -21,10 +21,10 @@ struct ListCardComponentView: View {
             }
             
             HStack{
-                Text("민지와 제주 여행")
+                Text("민지와 제주 여행") //viewmodel.title
                     .font(.custom("Pretendard", size: 20))
                     .fontWeight(.bold)
-                    .foregroundColor(Color.packitPurple)
+                    .foregroundColor(isBeen ? .black : Color.packitPurple)
                 Spacer()
             }
             
@@ -33,7 +33,7 @@ struct ListCardComponentView: View {
             HStack{
                 Spacer()
 
-                Text("제주도")
+                Text("제주도") //viewmodel.location
                     .font(.custom("Pretendard", size: 15))
                     .fontWeight(.ultraLight)
                     .foregroundColor(.black)
@@ -41,7 +41,7 @@ struct ListCardComponentView: View {
             HStack{
                 Spacer()
 
-                Text("2박 3일 일정")
+                Text("2박 3일 일정") //viewmodel.enddate~startdate counting?
                     .font(.custom("Pretendard", size: 15))
                     .fontWeight(.ultraLight)
                     .foregroundColor(.black)
@@ -65,11 +65,13 @@ struct ListCardComponentView: View {
                 .stroke(Color.packitPurple)
         )
         .overlay(
-            Image("packagePlus")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 35, height: 35)
-                .padding(10),
+            Group {
+                Image(isBeen ? "package" : "packagePlus")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 35, height: 35)
+                    .padding(10)
+            },
             alignment: .bottomLeading
         )
 
